fb1f053621156f58760de6e8bccc3b1fd3f24b38
parknahyun <nahyun75@gmail.com>
2023-03-10 00:45:41 +0900

    오류수정

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,11 +1,16 @@
+import logging
+
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
 
+import logging
+logger = logging.getLogger('pybo')
 
 def index(request):
+    logger.info("INFO 레벨로 출력")
     page=request.GET.get('page','1')
     kw = request.GET.get('kw', '')
     question_list=Question.objects.order_by('-create_date')
