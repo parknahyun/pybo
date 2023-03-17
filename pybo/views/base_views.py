1f5697b090c6655b3ad252c522b18144a8e3ef05
parknahyun <nahyun75@gmail.com>
2023-03-17 23:11:43 +0900

    __name__ add

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, get_object_or_404
 from ..models import Question
 
 import logging
-logger = logging.getLogger('pybo')
+logger = logging.getLogger(__name__)
 
 def index(request):
     logger.info("INFO 레벨로 출력")
